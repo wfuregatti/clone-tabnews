470b80b9eec15e9192e01838ef7d0f5f52efefd6
WANDER MARTINS <55296340+wfuregatti@users.noreply.github.com>
2023-10-31 14:37:15 +0000

    adding scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
 function Home() {
-    return <h1>“Any sufficiently advanced technology is indistinguishable from magic.”</h1>
+  return (
+    <h1>
+      “Any sufficiently advanced technology is indistinguishable from magic.”
+    </h1>
+  );
+}
+
+function test() {
+  console.log("test");
+}
+
+function test2() {
+  console.log("indentacao errada");
 }
 
 export default Home;
