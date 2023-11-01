ef1068d33a260ac88dbf4cfca059a6105b229a93
WANDER MARTINS <55296340+wfuregatti@users.noreply.github.com>
2023-11-01 15:20:26 +0000

    changing the quote slightly

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,9 @@
 function Home() {
   return (
     <h1>
-      “Any sufficiently advanced technology is indistinguishable from magic.”
+      Any sufficiently advanced technology is indistinguishable from magic.
     </h1>
   );
-}
-
-function test() {
-  console.log("test");
-}
-
-function test2() {
-  console.log("indentacao errada");
 }
 
 export default Home;
