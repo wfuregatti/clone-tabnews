f2bd8e5d9b492b40ba42bdff93155af3c231ab69
WANDER MARTINS <55296340+wfuregatti@users.noreply.github.com>
2023-12-05 16:50:46 +0000

    changing the json response on status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,7 +3,7 @@ import database from "infra/database.js";
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum;");
   console.log(result.rows);
-  response.status(200).json({ status: "API OK" });
+  response.status(200).json({ status: "API OK / Database OK" });
 }
 
 export default status;
