d788c40beede4e154687f121fa8ff49baade42e4
WANDER MARTINS <55296340+wfuregatti@users.noreply.github.com>
2025-03-11 20:20:23 +0000

    Single change to test commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      Once a new technology rolls over you, if you're not part of the
-      steamroller, you're part of the road.
-    </h1>
-  );
+  return <h1>Once a new technology rolls over you, if you are not part of the steamroller, you are part of the road.</h1>;
 }
 
 export default Home;
